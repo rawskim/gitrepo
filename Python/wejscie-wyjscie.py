9aa9769c9acf941da522b4675ed7bcf4665f5dd5
rawskim <michalrawski01@gmail.com>
2018-09-12 10:29:14 +0200

    programowanie c++

---
+++ Python/wejscie-wyjscie.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  szablon.py
+#
+
+
+def main(args):
+	a = int(input('Podaj 1. liczbę: '))
+	print(a)
+	b = int(input('Podaj 2. liczbę: '))
+	print(b)
+	
+	print('Suma: ', a + b)
+	print('Roznica: ', a - b)
+	print('Iloczyn: ', a * b)
+	print('Iloraz: ', a / b)
+	
+	return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
