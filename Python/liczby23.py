e614db78eaae8d4378986dac2b0b14912ce37e9b
rawskim <michalrawski01@gmail.com>
2018-10-10 19:11:51 +0200

    liczby 3cyfrowe

--- Python/liczby23.py
+++ Python/liczby23.py
@@ -2,26 +2,42 @@
 # -*- coding: utf-8 -*-
 #
 #  liczby23.py
-#  
+
 
 def liczby2():
+    
     """
-    Funkcja drukuje wszystkie liczby 2-cyfrowe,
-    których liczby nie powtarzają się. Funkcja zwraca ich liczbę.
+    Funkcja drukuje wszystkie liczby dwucyfrowe, 
+    w których cyfry nie powtarzają się. Funkcja zwraca ich liczbę.
     Wykluczone liczby: 11, 22, 33 itd.
     """
     ile = 0
-    for i in range(1, 10):
-        for j in range(0, 10):
-            if i != j:
-                print("{}{} ".format(i, j), end='')
+    for i in range(1,10):
+        for j in range(0,10):
+            if i != j: 
+                print("{}{} ".format(i,j), end='')
                 ile = ile + 1
-        return ile
-    
-    
+    return ile
+
+    """
+    Funkcja drukuje wszystkie liczby trzycyfrowe, 
+    w których cyfry nie powtarzają się. Funkcja zwraca ich liczbę.
+    Wykluczone liczby: 112, 224, 333 itd.
+    """ 
+def liczby3():
+	
+	ile = 0
+	for s in range(1,10):
+		for d in range(0,10):
+			for j in range(0,10):
+				if (s != d and s != j) and (d != s and d != j) and (j != s and j != d):
+					print("{}{}{} ".format(s,d,j), end='')
+					ile = ile + 1         
+	return ile	
+
 def main(args):
-    print("\n\nLiczb 2-cyfrowych: ", liczby2())
-    
+    print("Liczb 2-cyfrowych: ", liczby2())
+    print("Liczb 3-cyfrowych: ", liczby3())
     return 0
 
 if __name__ == '__main__':
