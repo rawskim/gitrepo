9881fb18a9af886506cf8679dcbc13c5905c0e1a
rawskim <michalrawski01@gmail.com>
2018-10-17 10:36:30 +0200

    c++

--- Python/tabliczka.py
+++ Python/tabliczka.py
@@ -4,14 +4,16 @@
 #  tabliczka.py
 #  
 
-def tabliczka():
-    for i in range(1,11):
-        for j in range(1,11):
-            print("{:>3} ".format(i*j), end='')
+def tabliczka(a, b):
+    for i in range(1, a + 1):
+        for j in range(1, b + 1):
+            print("{:>4} ".format(i*j), end='')
         print()
 
 def main(args):
-    tabliczka()
+    a = int(input("Podaj zakres 1: "))
+    b = int(input("Podaj zakres 2: "))
+    tabliczka(a, b)
     return 0
 
 if __name__ == '__main__':
