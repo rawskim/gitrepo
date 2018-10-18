03614e86bc46d8b438b273bdec4b675de9d6a186
rawskim <michalrawski01@gmail.com>
2018-10-18 08:45:24 +0200

    c++

---
+++ CPP/figury.cpp
@@ -0,0 +1,33 @@
+/*
+ * figury.cpp
+ */
+
+#include <iostream>
+
+using namespace std;
+
+void prostokat2(int a, int b, znak) {
+    for (int i = 1; i <= a; i++;) {
+        for (int j = 1; j <= b; j++) {
+            if ()
+        }
+    }
+}
+
+int main(int argc, char **argv)
+{
+    int a, b; // deklareacja
+    a = b = 0; // inicjacja
+    cout << "Podaj bok 1: ";
+    cin >> a;
+    cout << "Podaj bok 2: ";
+    cin >> b;
+    
+    char znak;
+    cout << "Podaj znak wypełnienia: ";
+    cin >> znak;
+    
+    prostokat2(a, b, znak);
+    return 0;
+}
+
