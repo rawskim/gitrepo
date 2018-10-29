0c9516e33cbebd0b4db4e23e554e291ba8b2eb2e
rawskim <michalrawski01@gmail.com>
2018-10-29 08:45:59 +0100

    kwerendy

--- Bazy/uczniowie/baza.py
+++ Bazy/uczniowie/baza.py
@@ -6,17 +6,18 @@ import csv
 import os.path
 
 def czy_jest(plik):
-    """Funkcja sprawdza czy plik jest na dysku"""
+    '''Funkcja sprawdza czy plik istnieje na dysku '''
     if not os.path.isfile(plik):
-        print("Plik {} nie istnieje".format(plik))
+        print("Plik {} nie istnieje!".format(plik))
         return False
     return True
+            
 
 def dane_z_pliku(nazwa_pliku, separator=','):
     dane = []  # pusta lista na dane
     if not czy_jest(nazwa_pliku):
-        return dane
-    
+        return dane 
+
     with open(nazwa_pliku, 'r', newline='', encoding='utf-8') as plik:
         tresc = csv.reader(plik, delimiter=separator)
         for rekord in tresc:
@@ -43,44 +44,48 @@ def kwerenda_1(cur):
         print(tuple(row))  # drukowanie pól
 
 def ile_kolumn(cur, tab):
+    """Funkcja sprawdza i zwraca liczbe kolumn w podanej tabeli"""
     licznik = 0
     for kol in cur.execute("PRAGMA table_info('" + tab + "')"):
-        licznik += 1
+        licznik += 1 
     return licznik
 
+
 def main(args):
-    #KONFIGURACJA #######
+    # KONFIGURACJA #######
     baza_nazwa = 'uczniowie'
     tabele = ['uczniowie', 'klasy', 'przedmioty', 'oceny']
     roz = '.csv'
-    
+    #####################
+
     con = sqlite3.connect(baza_nazwa + '.db')  # połączenie z bazą
     cur = con.cursor()  # utworzenie kursora
 
     # utworzenie tabeli w bazie
     if not czy_jest(baza_nazwa + '.sql'):
         return 0
+
     with open(baza_nazwa + '.sql', 'r') as plik:
         cur.executescript(plik.read())
 
-    # dodawanie danych do bazy
+    # dodawanie danych
     for tab in tabele:
         ile = ile_kolumn(cur, tab)
         dane = dane_z_pliku(tab + roz, separator=',')
         ile_d = len(dane[0])
         
-        if ile > ile_d:
-            dane2 = []
+        if ile > ile_d: #trzeba dodac NONE 
+            dane2 = [] # tymczasowa lista
             for r in dane:
                 r.insert(0, None)
                 dane2.append(r)
             dane = dane2
-    
-    ile = len(dane[0])
-        
-    cur.executemany('INSERT INTO ' + tab + 
-        ' VALUES(' + ','.join(['?'] * ile) + ')', dane)
-        
+            
+        ile = len(dane[0])
+                
+        cur.executemany('INSERT INTO ' + tab + 
+            ' VALUES(' + ','.join(['?'] * ile) + ')', dane)
+
     con.commit()  # zatwierdzenie zmian w bazie
     con.close()  # zamknięcie połączenia z bazą
     return 0
--- Bazy/uczniowie/uczniowie.sql
+++ Bazy/uczniowie/uczniowie.sql
@@ -1,48 +1,46 @@
 DROP TABLE IF EXISTS uczniowie;
+DROP TABLE IF EXISTS klasy;
+DROP TABLE IF EXISTS przedmioty;
+DROP TABLE IF EXISTS oceny;
 
 CREATE TABLE uczniowie
 (
-id INTEGER PRIMARY KEY NOT NULL, 
-imie TEXT(20),
-nazwisko TEXT(20), 
-plec BOOLEAN,
-id_klasa INTEGER NOT NULL,
-egz_hum NUMERIC NOT NULL DEFAULT 0,
-egz_mat NUMERIC NOT NULL DEFAULT 0,
-egz_jez NUMERIC NOT NULL DEFAULT 0,
-FOREIGN KEY (id_klasa) REFERENCES klasy(id)
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    imie TEXT,
+    nazwisko TEXT,
+    plec BOOLEAN,
+    id_klasa INTEGER NOT NULL,
+    egz_hum NUMERIC NOT NULL DEFAULT 0,
+    egz_mat NUMERIC NOT NULL DEFAULT 0,
+    egz_jez NUMERIC NOT NULL DEFAULT 0,
+    FOREIGN KEY (id_klasa) REFERENCES klasy(id)
+    ON DELETE CASCADE ON UPDATE NO ACTION
 );
-
-DROP TABLE IF EXISTS klasy;
 
 CREATE TABLE klasy
-( 
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-klasa TEXT(2),
-rok_naboru INTEGER,
-rok_matury INTEGER
+(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    klasa TEXT,
+    rok_naboru INTEGER,
+    rok_matury INTEGER
 );
-
-DROP TABLE IF EXISTS przedmioty;
 
 CREATE TABLE przedmioty
 (
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-przedmiot TEXT (20),
-imie_naucz TEXT (20),
-nazwisko_naucz TEXT (20),
-plec_naucz BOOLEAN
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    przedmiot TEXT,
+    imie_naucz TEXT,
+    nazwisko_naucz TEXT,
+    plec_naucz BOOLEAN
 );
-
-DROP TABLE IF EXISTS oceny;
 
 CREATE TABLE oceny
 (
-id INTEGER PRIMARY KEY NOT NULL,
-datad DATE,
-id_uczen INTEGER,
-id_przedmiot INTEGER
-ocena INTEGER,
-FOREIGN KEY (id_uczen) REFERENCES uczniowie(id),
-FOREIGN KEY (id_przedmiot) REFERENCES przedmioty(id)
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    datad DATA,
+    id_uczen INTEGER NOT NULL,
+    id_przedmiot INTEGER NOT NULL,
+    ocena DECIMAL NOT NULL,
+    FOREIGN KEY (id_uczen) REFERENCES uczniowie(id)
+    FOREIGN KEY (id_przedmiot) REFERENCES przedmioty(id)
 );
