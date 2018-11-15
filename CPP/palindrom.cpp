8ec6b52f8944fbb0caa9a3c569efb9510ded1546
rawskim <michalrawski01@gmail.com>
2018-11-15 08:46:02 +0100

    fibonacci

--- CPP/palindrom.cpp
+++ CPP/palindrom.cpp
@@ -1,12 +1,17 @@
 /*
  * palindrom.cpp
  */
-
+ 
 #include <iostream>
 #include <string.h>
 
 using namespace std;
 
+int zlicz(char tab[]) {
+    int i = 0;
+    while(tab[i] != '\0') i++;
+    return i;
+}
 
 bool palindrom(char tab[], int roz) {
     bool czyPal = true;
@@ -21,17 +26,16 @@ bool palindrom(char tab[], int roz) {
     return czyPal;
 }
 
-
 int main(int argc, char **argv)
 {
-    int roz = 20;
-    char wyraz[roz];
-    cout << "Podaj wyraz: ";
-    cin.getline(wyraz, roz);
-    if (palindrom(wyraz, strlen(roz)))
+    const int rozmiar = 20;  // deklaracja stałej
+    char tekst[rozmiar];  // deklaracja tablicy znakowej
+    cout << "Podaj wyraz:\n";
+    cin.getline(tekst, rozmiar);
+    // cin.gcount()-1
+    if (palindrom(tekst, strlen(tekst)))
         cout << "Palindrom!";
     else
-        cout << "Nie palindrom";
+        cout << "The thing";
     return 0;
 }
-
