351b33f7e4eb42cd7eaad4b46dde745a75236108
rawskim <michalrawski01@gmail.com>
2019-01-28 08:46:01 +0100

    quiz

--- CPP/szukaj.cpp
+++ CPP/szukaj.cpp
@@ -1,71 +1,79 @@
-/*
- * szukaj.cpp
- */
-
-
 #include <iostream>
 using namespace std;
 
-void wypelnij(int tab[], int roz) {
-    srand(time(NULL));  // inicjacja generatora liczb pseudolosowych
-    for(int i = 0; i < roz; i++) {
+void wypelnij(int tab[], int rozmiar) {
+    srand(time(NULL)); // inicjacja generatora liczb pseudolosowych
+    for(int i=0; i < rozmiar; i++) {
+        // cout << rand() << endl;
         tab[i] = rand() % 101;
     }
 }
 
-void drukuj(int tab[], int roz) {
-    for(int i = 0; i < roz; i++) {
+void drukuj(int tab[], int rozmiar) {
+    for(int i=0; i < rozmiar; i++) {
         cout << tab[i] << " ";
     }
+    cout << endl;
 }
 
-void sort_insert(int tab[], int n) {
+void sort_insert(int tab[],int n) {
     int i, k, el;
-    for (i = 1; i < n; i++) {
+    for(i = 1; i < n; i++) {
         el = tab[i];
-        k = i - 1;
-        while (k >= 0 && tab[k] > el) {
-            tab[k + 1] = tab[k];
+        k = i - 1; // indeks najmniejszego elementu
+        while (k>=0 && tab[k]>el){
+            tab[k+1]=tab[k];
             k--;
         }
-        tab[k+1] = el;
-    }
-}
+    }   tab[k+1]= el;
 
-int szukaj_lin(int tab[], int n, int szuk) {  // dziel i zwyciężaj
-    for(int i = 0; i < n; i++)
-        if (tab[i] == szuk)
+}
+// algorytm 
+int szukaj_lin(int tab[], int n, int szuk) {
+    for(int i=0; i<n; i++)
+        if (tab [i] == szuk)
             return i;
     return -1;
 }
 
 int szukaj_bin_lin(int tab[], int n, int szuk) {
-    int p, k, s;
-    p = 0; k = n - 1;
+    int p, k, s, w;
+    p = 0; k = n -1;
     while (p <= k) {
-        s = (p+k) / 2;
-        if (tab[s] == szuk) {
-            p = s;
-            break;
-        } else if (szuk < tab[s]) k = k -1;
-        else p = s + 1;
+        s = (p + k) / 2;
+        if(tab[s] == szuk) {
+                w = s;
+                break;
+        } else if (szuk < tab[s]) k = s - 1;
+        else p = s + 1; 
     }
+    return w;
     
-    return p;
+}
+
+int szukaj_bin_rek(int tab[], int szuk, int p, int k) {
+    if (p<=k) {
+        int s = (p+k) / 2;
+        if (tab[s] == szuk) return s;
+        else if (szuk < tab[s]) return szukaj_bin_rek(tab, szuk, p, s-1);
+        }
 }
 
 int main(int argc, char **argv)
 {
     int n = 20;
     int tab[n];
-    int szuk = 0;
     wypelnij(tab, n);
     drukuj(tab, n);
+    int szuk = 0;
     cout << "Podaj liczbę: "; cin >> szuk;
-    sort_insert(tab, n);
-    int indeks = szukaj_bin_lin(tab, n, szuk);
-    if (indeks >= 0) cout << "Znaleziono: " << endl << endl;
-    else cout << "Nie znaleziono";
+    sort_insert(tab ,n);
+    
+    int indeks = szukaj_bin_rek(tab, szuk, 0, n-1);
+    if (indeks>=0) cout << "Znaleziono: " << indeks << endl;
+    else cout << "Nie znaleziono"; 
+    
+    //~ cout << "Podaj liczbę:"; cin >> szuk;
     //~ int indeks = szukaj_lin(tab, n, szuk);
     //~ if (indeks != -1)
         //~ cout << "Znaleziono!";
