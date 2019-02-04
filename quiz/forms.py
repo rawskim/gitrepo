77f65e0f2ba925686588f360016ab107b76b8636
rawskim <michalrawski01@gmail.com>
2019-02-04 08:45:32 +0100

    quiz

--- quiz/forms.py
+++ quiz/forms.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# forms.py
+#  forms.py
 
 from flask_wtf import FlaskForm
 from wtforms import HiddenField, StringField, BooleanField
 from wtforms import SelectField, FormField, FieldList
 from wtforms.validators import Required
+
 
 blad1='To pole jest wymagane!'
 
@@ -19,5 +20,6 @@ class OdpForm(FlaskForm):
 class DodajForm(FlaskForm):
     id = HiddenField()
     pytanie = StringField('Treść pytania:', validators=[Required(message=blad1)])
-    kategorie = SelectField('Kategoria', coerce=int)
+    kategoria = SelectField('Kategoria', coerce=int)
     odpowiedzi = FieldList(FormField(OdpForm), min_entries=3)
+
--- quiz/views.py
+++ quiz/views.py
@@ -8,12 +8,11 @@ from modele import Kategoria, Pytanie, Odpowiedz
 from forms import *
 
 
-
 app = Flask(__name__)
 
-# widok domysllny
+# widok domyślny
 @app.route("/")
-def hello():
+def index():
     return render_template('index.html')
 
 @app.route("/lista")
@@ -30,19 +29,70 @@ def quiz():
         for pid, oid in request.form.items():
             if Odpowiedz().get(Odpowiedz.id == int(oid)).odpok:
                 wynik += 1
-                
+        
         flash('Poprawnych odpowiedzi: {}'.format(wynik), 'info')
-        return redirect(url_for('hello'))
-    
+        return redirect(url_for('index'))
+                
     pytania = Pytanie.select().join(Odpowiedz).distinct()
     return render_template('quiz.html', pytania=pytania)
 
+
+def flash_errors(form):
+    """Odczytanie wszystkich błędów formularza i przygotowanie komunikatów"""
+    for field, errors in form.errors.items():
+        for error in errors:
+            if type(error) is list:
+                error = error[0]
+            flash("Błąd: {}. Pole: {}".format(
+                error,
+getattr(form, field).label.text))
+
+
 @app.route("/dodaj", methods=['GET', 'POST'])
 def dodaj():
-    
+    """Dodawanie pytań i odpowiedzi"""
     form = DodajForm()
+    form.kategoria.choices = [(k.id, k.kategoria) for k in Kategoria.select()]
+    
+    if form.validate_on_submit():
+        print (form.data)
+        p = Pytanie(pytanie=form.pytanie.data, kategoria=form.kategoria.data)
+        p.save()
+        for odp in form.odpowiedzi.data:
+            odp = Odpowiedz(odpowiedz=odp['odpowiedz'],
+                            pytanie=p.id,
+                            odpok=int(odp['odpok']))
+            odp.save()
+        flash("Dodano pytanie: {}".format(form.pytanie.data))
+        return redirect(url_for('lista'))
+        
+    elif request.method == 'POST':
+        flash.errors(form)
+    
     return render_template('dodaj.html', form=form)
 
+
+def get_or_404(pid):
+    try:
+        p = Pytanie.get_by_id(pid)
+        return p
+    except Pytanie.DoesNotExist:
+        abort(404)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.route("/usun/<int:pid>", methods=['GET', 'POST'])
+def usun():
+    """Usuwanie pytan i odpowiedzi"""
+    p = get_or_404(pid)
+    if request.method == 'POST':
+        
+    return render
+        
 def main(args):
     return 0
 
